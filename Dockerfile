df9b30773d55f4c7abd9cc2035e105e9aa2c0375
chetasr <thephantomblu@gmail.com>
2020-10-13 15:32:29 +0400

    Fix webhook

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,8 @@ WORKDIR /workspace
 RUN pip install -r requirements.txt
 
 EXPOSE 80
+EXPOSE 443
+EXPOSE 88
+EXPOSE 8443
 
 CMD python ./app.py
--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     # Start the Bot
-    updater.start_webhook(webhook_url='https://master-media-dl-chetasr.endpoint.ainize.ai')
+    updater.start_webhook()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
